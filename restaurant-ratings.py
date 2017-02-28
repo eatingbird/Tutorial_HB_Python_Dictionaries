bcb6417ed50d035cb7dda41b7a875b28be19f232
Hackbright Student <no-reply@hackbrightacademy.com>
2017-02-27 17:09:59 -0800

    Cleaned up code.

--- restaurant-ratings.py
+++ restaurant-ratings.py
@@ -18,8 +18,8 @@ def get_ratings_from_file(ratings):
 def print_restaurant_ratings(ratings):
     """Prints restaurants in alphabetical order with their ratings."""
 
-    for restaurant in sorted(ratings.keys()):
-        print '%s is rated at %d' % (restaurant, ratings[restaurant])
+    for restaurant, rating in sorted(ratings.items()):
+        print '%s is rated at %d' % (restaurant, rating)
 
 
 def get_ratings_from_user(ratings):
@@ -28,9 +28,7 @@ def get_ratings_from_user(ratings):
     Sets restuarant and ratings as key value pairs to supplied dictionary.
     """
 
-    prompt_user = True
-
-    while prompt_user:
+    while True:
 
         restaurant = raw_input("Enter the name of the restaurant: ")
         rating = int(raw_input("Enter the rating of the restaurant: "))
@@ -40,14 +38,15 @@ def get_ratings_from_user(ratings):
 
         # Ask user if they want to keep entering restaurants and ratings
         user_choice = raw_input("Do you want to add another restaurant? y/n ").lower()
+        print user_choice
 
-        if user_choice is 'y':
+        if user_choice == 'y':
             continue
-        elif user_choice is 'n':
-            prompt_user = False
+        elif user_choice == 'n':
+            break
         else:
             print "The answer was supposed to be y or n. We will print the result thus far."
-            prompt_user = False
+            break
 
 
 get_ratings_from_file(ratings)
